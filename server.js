86ffb1bb120904355732f00d11b29fd738d3d357
LutherMckeiver <leoluther96@gmail.com>
2018-06-25 17:37:02 -0700

    added gheroku database and beginner routes

--- server.js
+++ server.js
@@ -5,18 +5,37 @@ const express = require('express');
 const cors = require('cors');
 const pg = require('pg');
 
+
+// Constring 
+// const conStringTrav = `postgres://postgres:1234@localhost:5432/`
+const conString = `postgres://localhost:5432/omghalp_app`
+const client = new pg.Client(conString);
+client.connect();
+client.on(`error`, err => {
+    console.error(err);
+});
+
+
+
+
 // App Setup
 const app = express();
 const PORT = process.env.PORT;
 
-// Database
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('./public'));
 
 // Middleware
 app.use(cors());
 
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/problems', (req, res) => res.send('Hello World!'));
+
+app.post('/question')
+app.put('/solution')
+
+app.delete()
 // Server Listen to Request
 app.listen(PORT, () => console.log(`Your server works on ${PORT} dude!`));
 
