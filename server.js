c6a51264790ccb722e84027209dbb570891b26e7
Travis Williams <travisjw@gmail.com>
2018-06-26 17:51:03 -0700

    get and post routes now confirmed to work

--- server.js
+++ server.js
@@ -5,37 +5,73 @@ const express = require('express');
 const cors = require('cors');
 const pg = require('pg');
 
+//Ubuntu
+const conString = `postgres://postgres:1234@localhost:5432/omghalpproject`
 
-// Constring 
-// const conStringTrav = `postgres://postgres:1234@localhost:5432/`
-const conString = `postgres://localhost:5432/omghalp_app`
+//Mac
+// const conString = `postgres://localhost:5432/omghalp_app`
+
 const client = new pg.Client(conString);
 client.connect();
-client.on(`error`, err => {
-    console.error(err);
-});
-
-
+// client.on(`error`, err => {
+//     console.error(err);
+// });
 
 
 // App Setup
 const app = express();
 const PORT = process.env.PORT;
 
-app.use(express.json());
+// app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('./public'));
 
 // Middleware
 app.use(cors());
 
+app.get('/problem', (req, res) => {
+    let SQL = 'SELECT * FROM problem'
+    
+    client.query(SQL)
+    .then(function (result) {
+        res.send(result.rows);
+        })
+        .catch(function (err) {
+          console.error(err);
+        });
+    });
 
-app.get('/problems', (req, res) => res.send('Hello World!'));
+app.post('/question', (req, res) => {
 
-app.post('/question')
-app.put('/solution')
+    let SQL = `INSERT INTO problem (question, tag, expectation, best_guess, code, result)
+    VALUES($1, $2, $3, $4, $5, $6);
+    `;
 
-app.delete()
+    let values = [
+        req.body.question,
+        req.body.tagz,
+        req.body.expectation,
+        req.body.best_guess,
+        req.body.code,
+        req.body.result
+    ]
+
+    client.query(SQL, values)
+        .then(function () {
+            console.log("made it to the .then of the get request")
+            res.send('insert complete')
+        })
+        .catch(function (err) {
+            console.error(err);
+        });
+})
+
+
+
+
+// app.put('/solution')
+
+// app.delete()
+
+
 // Server Listen to Request
 app.listen(PORT, () => console.log(`Your server works on ${PORT} dude!`));
-
